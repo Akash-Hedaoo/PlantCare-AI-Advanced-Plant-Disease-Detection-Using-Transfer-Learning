ba094be6851971a90e69edb6ca6439549ed2175f
aniketjawarkar <aniketnot2026@gmail.com>
2026-03-07 08:08:25 +0530

    Refactor dataset directory paths for consistency in config file

--- 2_Model_Building/config.py
+++ 2_Model_Building/config.py
@@ -2,8 +2,9 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-TRAIN_DIR = os.path.join(BASE_DIR, "1_Data_Collection_and_Preprocessing", "train")
-VALID_DIR = os.path.join(BASE_DIR, "1_Data_Collection_and_Preprocessing", "valid")
+TRAIN_DIR = "../1_Data_Collection_and_Preprocessing/train"
+VALID_DIR = "../1_Data_Collection_and_Preprocessing/valid"
+TEST_DIR  = "../1_Data_Collection_and_Preprocessing/test"
 
 # Model & training constants
 IMG_SIZE = (224, 224)
